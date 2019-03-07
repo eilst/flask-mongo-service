e2767104e08c66cc9545d394991dab03e91a123f
Luis <eilstdq@gmail.com>
2019-03-07 15:52:10 -0500

    add GET task id and POST task

--- flask-app/run.py
+++ flask-app/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect,  jsonify, request
+from flask import Flask, render_template, redirect, jsonify, request
 from pymongo import MongoClient
 from classes import *
 
@@ -10,28 +10,35 @@ db = client.TaskManager
 
 if db.settings.find({'name': 'task_id'}).count() <= 0:
     print("task_id Not found, creating....")
-    db.settings.insert_one({'name':'task_id', 'value':0})
+    db.settings.insert_one({'name': 'task_id', 'value': 0})
+
 
 def updateTaskID(value):
     task_id = db.settings.find_one()['value']
     task_id += value
     db.settings.update_one(
-        {'name':'task_id'},
+        {'name': 'task_id'},
         {'$set':
-            {'value':task_id}
-        })
+            {'value': task_id}
+         })
+
 
 def createTask(form):
     title = form.title.data
     priority = form.priority.data
     shortdesc = form.shortdesc.data
     task_id = db.settings.find_one()['value']
-    
-    task = {'id':task_id, 'title':title, 'shortdesc':shortdesc, 'priority':priority}
+
+    task = {
+        'id': task_id,
+        'title': title,
+        'shortdesc': shortdesc,
+        'priority': priority}
 
     db.tasks.insert_one(task)
     updateTaskID(1)
     return redirect('/')
+
 
 def deleteTask(form):
     key = form.key.data
@@ -39,45 +46,82 @@ def deleteTask(form):
 
     if(key):
         print(key, type(key))
-        db.tasks.delete_many({'id':int(key)})
+        db.tasks.delete_many({'id': int(key)})
     else:
-        db.tasks.delete_many({'title':title})
+        db.tasks.delete_many({'title': title})
 
     return redirect('/')
+
 
 def updateTask(form):
     key = form.key.data
     shortdesc = form.shortdesc.data
-    
+
     db.tasks.update_one(
         {"id": int(key)},
         {"$set":
             {"shortdesc": shortdesc}
-        }
+         }
     )
 
     return redirect('/')
 
+
 def resetTask(form):
     db.tasks.drop()
     db.settings.drop()
-    db.settings.insert_one({'name':'task_id', 'value':0})
+    db.settings.insert_one({'name': 'task_id', 'value': 0})
     return redirect('/')
 
-@app.route('/task', methods=['GET'])
-def get_all_frameworks():
-    framework = db.tasks
 
+@app.route('/tasks', methods=['GET'])
+def get_all_tasks():
+    tasks = db.tasks
     output = []
+    for q in tasks.find():
+        output.append({'id': q['id'],
+                       'title': q['title'],
+                       'shortdesc': q['shortdesc'],
+                       'priority': q['priority']})
 
-    for q in framework.find():
-        output.append({'title' : q['title'], 'shortdesc' : q['shortdesc'], 'priority' : q['priority']})
-
-    return jsonify({'result' : output})
+    return jsonify({'result': output})
 
 
+@app.route('/task/<id>', methods=['GET'])
+def get_one_task(id):
+    tasks = db.tasks
+    q = tasks.find_one({'id': int(id)})
+    if q:
+        output = {
+            'id': q['id'],
+            'title': q['title'],
+            'shortdesc': q['shortdesc'],
+            'priority': q['priority']}
+    else:
+        output = 'No results found'
+    return jsonify({'result': output})
 
-@app.route('/', methods=['GET','POST'])
+
+@app.route('/task', methods=['POST'])
+def add_tasks():
+    tasks = db.tasks
+
+    title = request.json['title']
+    task_id = db.settings.find_one()['value']
+    shortdesc = request.json['shortdesc']
+    priority = request.json['priority']
+    task = {
+        'id': task_id,
+        'title': title,
+        'shortdesc': shortdesc,
+        'priority': priority}
+    new_task = tasks.insert_one(task)
+    updateTaskID(1)
+    resp = jsonify(success=True)
+    return resp
+
+
+@app.route('/', methods=['GET', 'POST'])
 def main():
     # create form
     cform = CreateTask(prefix='cform')
@@ -101,8 +145,9 @@ def main():
     for i in docs:
         data.append(i)
 
-    return render_template('home.html', cform = cform, dform = dform, uform = uform, \
-            data = data, reset = reset)
+    return render_template('home.html', cform=cform, dform=dform, uform=uform,
+                           data=data, reset=reset)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
